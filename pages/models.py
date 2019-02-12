9870a5ca8f3ce7f1353a9326f219043237084ac7
abdullah <anabanmi@hotmail.com>
2019-02-12 13:21:06 +0300

    atallah and abdullah a

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Page(models.Model):
+    title =  models.CharField(max_length=50)
+    content =  models.TextField()
+    last_update =  models.DateTimeField()
+
+    def get_absolute_url(self):
+    	# return "/detail/%i/" %(self.id)
+
+    	return reverse('page-detail', kwargs={'page_id': self.id})
+
+
+    def __str__(self):
+    	return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Page
 # Create your views here.
+
+
+
+def list(request):
+	context = {
+		"subjects": Page.objects.all()
+	}
+
+	return render(request, 'list.html', context)
+
+
+def detail(request, page_id):
+	context = {
+		"subject": Page.objects.get(id = page_id)
+	}
+	
+	return render(request, 'detail.html', context)
